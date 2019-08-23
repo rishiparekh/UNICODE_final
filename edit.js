df530a401461e880ae6ed018789d406e2236eaa3
rishiparekh <54431382+rishiparekh@users.noreply.github.com>
2019-08-23 13:15:59 +0530

    Add files via upload

---
+++ edit.js
@@ -0,0 +1,34 @@
+var myform=document.forms.edit;
+myform.email.value;
+
+var myform=document.forms.edit;
+myform.email.value;
+var new_email=document.email_field;
+
+var new_email=document.getElementById("email_field");
+var new_number=document.getElementById("number_field");
+var key=document.getElementById("submit");
+key.onclick=function(){
+    alert("hey");
+}
+
+    new_email.innerHTML=myform.email.value;
+    new_number.innerHTML=myform.number.value;
+
+
+​
+
+
+
+
+
+/*var modal=document.getElementById('edit');
+
+           var modalBtn=document.getElementById('button');
+            
+            modalBtn.addEventListener('click',openmodal);
+
+            function openmodal(){
+                modal.style.display='block';
+                
+            }*/
